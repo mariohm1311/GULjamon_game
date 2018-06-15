e8ffa5a8e39383f8e97baca254f4c4e47e321457
Miiaan <miguelantohi@hotmail.com>
2018-06-15 12:09:05 +0200

    Perfect movement

--- assets/characters/scene/character2.gd
+++ assets/characters/scene/character2.gd
@@ -1,24 +1,67 @@
 extends KinematicBody2D
 
-
 export var vel_scalar = 300
 export var jump = 1000
 export var g_force = 20
 var vel = Vector2()
 
+var flipped = false
+var moving = false
 
 func _physics_process(delta):
-	vel.x = 0
+	
 	vel.y += g_force
-	if(Input.is_action_pressed("go_left2")):
+		
+	if Input.is_action_pressed("go_left2") and is_on_floor():
+		vel.x = 0
+		flipped = true
+		moving = true
+		$AnimatedSprite.flip_h = flipped
+		if not $AnimatedSprite/AnimationPlayer.current_animation == "moving_ground":
+    		$AnimatedSprite/AnimationPlayer.play("moving_ground")
 		vel.x = -vel_scalar
-	if(Input.is_action_pressed("go_right2")):
+	
+	if Input.is_action_pressed("go_right2") and is_on_floor():
+		vel.x = 0
+		flipped = false
+		moving = true
+		$AnimatedSprite.flip_h = flipped
+		if not $AnimatedSprite/AnimationPlayer.current_animation == "moving_ground":
+    		$AnimatedSprite/AnimationPlayer.play("moving_ground")
 		vel.x = vel_scalar
-	if(Input.is_action_just_pressed("jump2") and is_on_floor()):
+	
+	if Input.is_action_pressed("jump2") and is_on_floor():
 		vel.y = -jump
-	if(Input.is_action_pressed("crouch1")):
+		moving = true
+	
+	if Input.is_action_pressed("go_right2") and not is_on_floor() and not vel.x >= vel_scalar:
+		vel.x += 10
+		flipped = false
+		moving = true
+	
+	if Input.is_action_pressed("go_left2") and not is_on_floor() and not vel.x <= -vel_scalar :
+		vel.x -= 10
+		flipped = true
+		moving = true
+			
+	if Input.is_action_pressed("crouch2"):
 		pass
-
+	
+	if is_on_floor() and not moving:
+		vel.x = 0
+		$AnimatedSprite/AnimationPlayer.play("static")
+	
+	elif not is_on_floor():
+		if vel.y <= 0:
+			$AnimatedSprite.flip_h = flipped
+			if not $AnimatedSprite/AnimationPlayer.current_animation == "moving_up":
+	    		$AnimatedSprite/AnimationPlayer.play("moving_up")
+		else:
+			$AnimatedSprite.flip_h = flipped
+			if not $AnimatedSprite/AnimationPlayer.current_animation == "moving_down":
+	    		$AnimatedSprite/AnimationPlayer.play("moving_down")
+		
+	moving = false
 
 	move_and_slide(vel, Vector2(0,-1))
 
